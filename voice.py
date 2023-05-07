21720653302816edf176700d3fff03fae08213b3
ArijSaaleh <arij.saleh@esprit.tn>
2023-05-07 12:35:48 +0100

    pv

--- voice.py
+++ voice.py
@@ -1,82 +1,288 @@
-import os
+from time import sleep
+from threading import Thread, Event
+from pvrecorder import PvRecorder
+from pvleopard import *
+from colorama import Fore, Style
+import pygame
+import boto3
 import openai
-from dotenv import load_dotenv
+import os
+import pvcobra
+import pvleopard
+import pvporcupine
+import pyaudio
+import random
+import struct
+import sys
+import textwrap
+import threading
 import time
-import speech_recognition as sr
-import pyttsx3
-import numpy as np
-# from os.path import join, dirname
-# import matplotlib.pyplot as plt
-# ^ matplotlib is great for visualising data and for testing purposes but usually not needed for production
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
-load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')
-model = 'gpt-3.5-turbo'
-# Set up the speech recognition and text-to-speech engines
-r = sr.Recognizer()
-engine = pyttsx3.init()
-voice = engine.getProperty('voices')[1]
-engine.setProperty('voice', voice.id)
-name = "RIRI"
-greetings = [f"whats up master {name}", 
-             "yeah?", 
-             "Well, hello there, Master of Puns and Jokes - how's it going today?",
-             f"Ahoy there, Captain {name}! How's the ship sailing?",
-             f"Bonjour, Monsieur {name}! Comment ça va? Wait, why the hell am I speaking French?" ]
 
-# Listen for the wake word "Tomato"
-def listen_for_wake_word(source):
-    print("Listening for 'Tomato'...")
+audio_stream = None
+cobra = None
+pa = None
+polly = boto3.client('polly', region_name='us-west-2')
+porcupine = None
+recorder = None
+wav_file = None
 
-    while True:
-        audio = r.listen(source)
-        try:
-            text = r.recognize_google(audio)
-            if "tomato" in text.lower():
-                print("Wake word detected.")
-                engine.say(np.random.choice(greetings))
-                engine.runAndWait()
-                listen_and_respond(source)
-                break
-        except sr.UnknownValueError:
-            pass
+# most capable GPT-3.5 model and optimized for chat
+GPT_model = "gpt-3.5-turbo-0301"
+# this will be supported until at least June 1
+# OpenAI will update gpt-3.5-turbo to a new stable release in April
 
-# Listen for input and respond with OpenAI API
-def listen_and_respond(source):
+
+prompt = ["How may I assist you?",
+          "How may I help?",
+          "What can I do for you?",
+          "Ask me anything.",
+          "Yes?",
+          "I'm here.",
+          "I'm listening.",
+          "What would you like me to do?"]
+
+chat_log = [
+    {"role": "system", "content": "You are a helpful assistant."},
+]
+
+
+def ChatGPT(query):
+    chat_log.append({"role": "user", "content": query})
+    response = openai.ChatCompletion.create(
+        model=GPT_model,
+        messages=chat_log
+    )
+    return str.strip(response['choices'][0]['message']['content'])
+    chat_log.append({"role": "system", "content": response})
+
+
+def responseprinter(chat):
+    for word in chat:
+        time.sleep(0.055)
+        print(word, end="", flush=True)
+    print()
+
+# DaVinci will 'remember' earlier queries so that it has greater continuity in its response
+# the following will delete that 'memory' five minutes after the start of the conversation
+
+
+def append_clear_countdown():
+    sleep(300)
+    global chat_log
+    chat_log.clear()
+    chat_log = [
+        {"role": "system", "content": "You are a helpful assistant."},
+    ]
+    global count
+    count = 0
+    t_count.join
+
+
+def voice(chat):
+
+    voiceResponse = polly.synthesize_speech(Text=chat, OutputFormat="mp3",
+                                            VoiceId="Matthew")  # other options include Amy, Joey, Nicole, Raveena and Russell
+    if "AudioStream" in voiceResponse:
+        with voiceResponse["AudioStream"] as stream:
+            output_file = "speech.mp3"
+            try:
+                with open(output_file, "wb") as file:
+                    file.write(stream.read())
+            except IOError as error:
+                print(error)
+
+    else:
+        print("did not work")
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(output_file)
+#    pygame.mixer.music.set_volume(0.8) # uncomment to control the the playback volume (from 0.0 to 1.0
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pass
+    sleep(0.2)
+
+
+def wake_word():
+
+    porcupine = pvporcupine.create(keywords=["snowboy", "jarvis", "DaVinci"],
+                                   access_key=pv_access_key)
+
+    devnull = os.open(os.devnull, os.O_WRONLY)
+    old_stderr = os.dup(2)
+    sys.stderr.flush()
+    os.dup2(devnull, 2)
+    os.close(devnull)
+
+    wake_pa = pyaudio.PyAudio()
+
+    porcupine_audio_stream = wake_pa.open(
+        rate=porcupine.sample_rate,
+        channels=1,
+        format=pyaudio.paInt16,
+        input=True,
+        frames_per_buffer=porcupine.frame_length)
+
+    Detect = True
+
+    while Detect:
+        porcupine_pcm = porcupine_audio_stream.read(porcupine.frame_length)
+        porcupine_pcm = struct.unpack_from(
+            "h" * porcupine.frame_length, porcupine_pcm)
+
+        porcupine_keyword_index = porcupine.process(porcupine_pcm)
+
+        if porcupine_keyword_index >= 0:
+
+            print(Fore.GREEN + "\nWake word detected\n")
+            porcupine_audio_stream.stop_stream
+            porcupine_audio_stream.close()
+            porcupine.delete()
+            os.dup2(old_stderr, 2)
+            os.close(old_stderr)
+            Detect = False
+
+
+def listen():
+
+    cobra = pvcobra.create(access_key=pv_access_key)
+
+    listen_pa = pyaudio.PyAudio()
+
+    listen_audio_stream = listen_pa.open(
+        rate=cobra.sample_rate,
+        channels=1,
+        format=pyaudio.paInt16,
+        input=True,
+        frames_per_buffer=cobra.frame_length)
+
     print("Listening...")
 
     while True:
-        audio = r.listen(source)
-        try:
-            text = r.recognize_google(audio)
-            print(f"You said: {text}")
-            if not text:
-                continue
+        listen_pcm = listen_audio_stream.read(cobra.frame_length)
+        listen_pcm = struct.unpack_from("h" * cobra.frame_length, listen_pcm)
 
-            # Send input to OpenAI API
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{text}"}]) 
-            response_text = response.choices[0].message.content
-            print(f"OpenAI response: {response_text}")
-
-            # Speak the response
-            engine.say(response_text)
-            engine.runAndWait()
-
-            if not audio:
-                listen_for_wake_word(source)
-        except sr.UnknownValueError:
-            time.sleep(2)
-            print("Silence found, shutting up, listening...")
-            listen_for_wake_word(source)
-            break
-            
-        except sr.RequestError as e:
-            print(f"Could not request results; {e}")
-            engine.say(f"Could not request results; {e}")
-            engine.runAndWait()
-            listen_for_wake_word(source)
+        if cobra.process(listen_pcm) > 0.3:
+            print("Voice detected")
+            listen_audio_stream.stop_stream
+            listen_audio_stream.close()
+            cobra.delete()
             break
 
-# Use the default microphone as the audio source
-with sr.Microphone() as source:
-    listen_for_wake_word(source)
+
+def detect_silence():
+
+    cobra = pvcobra.create(access_key=pv_access_key)
+
+    silence_pa = pyaudio.PyAudio()
+
+    cobra_audio_stream = silence_pa.open(
+        rate=cobra.sample_rate,
+        channels=1,
+        format=pyaudio.paInt16,
+        input=True,
+        frames_per_buffer=cobra.frame_length)
+
+    last_voice_time = time.time()
+
+    while True:
+        cobra_pcm = cobra_audio_stream.read(cobra.frame_length)
+        cobra_pcm = struct.unpack_from("h" * cobra.frame_length, cobra_pcm)
+
+        if cobra.process(cobra_pcm) > 0.2:
+            last_voice_time = time.time()
+        else:
+            silence_duration = time.time() - last_voice_time
+            if silence_duration > 1.3:
+                print("End of query detected\n")
+                cobra_audio_stream.stop_stream
+                cobra_audio_stream.close()
+                cobra.delete()
+                last_voice_time = None
+                break
+
+
+class Recorder(Thread):
+    def __init__(self):
+        super().__init__()
+        self._pcm = list()
+        self._is_recording = False
+        self._stop = False
+
+    def is_recording(self):
+        return self._is_recording
+
+    def run(self):
+        self._is_recording = True
+
+        recorder = PvRecorder(device_index=-1, frame_length=512)
+        recorder.start()
+
+        while not self._stop:
+            self._pcm.extend(recorder.read())
+        recorder.stop()
+
+        self._is_recording = False
+
+    def stop(self):
+        self._stop = True
+        while self._is_recording:
+            pass
+
+        return self._pcm
+
+
+try:
+
+    o = create(
+        access_key=pv_access_key,
+        enable_automatic_punctuation=True,
+    )
+
+    event = threading.Event()
+
+    count = 0
+
+    while True:
+
+        if count == 0:
+            t_count = threading.Thread(target=append_clear_countdown)
+            t_count.start()
+        else:
+            pass
+        count += 1
+        wake_word()
+# comment out the next line if you do not want DaVinci to respond to his name
+        voice(random.choice(prompt))
+        recorder = Recorder()
+        recorder.start()
+        listen()
+        detect_silence()
+        transcript, words = o.process(recorder.stop())
+        t_fade = threading.Thread(target=fade_leds, args=(event,))
+        t_fade.start()
+        recorder.stop()
+        print(transcript)
+#        voice(transcript) # uncomment to have DaVinci repeat what it heard
+        (res) = ChatGPT(transcript)
+        print("\nChatGPT's response is:\n")
+        t1 = threading.Thread(target=voice, args=(res,))
+        t2 = threading.Thread(target=responseprinter, args=(res,))
+
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
+        event.set()
+
+        recorder.stop()
+        o.delete
+        recorder = None
+
+except KeyboardInterrupt:
+    print("\nExiting ChatGPT Virtual Assistant")
+    o.delete
